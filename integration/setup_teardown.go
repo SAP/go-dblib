b27347872b459d5e2249f09e483c97f4ac0f46b6
Nelo-T. Wallus <nelo@wallus.de>
2021-10-15 10:07:15 +0000

    Rewrite integration setup/teardown to wait on current ASE processes

--- integration/setup_teardown.go
+++ integration/setup_teardown.go
@@ -8,8 +8,12 @@ package integration
 import (
 	"context"
 	"database/sql"
+	"errors"
 	"fmt"
+	"math"
+	"math/rand"
 	"sync"
+	"time"
 
 	"github.com/SAP/go-dblib/dsn"
 )
@@ -21,82 +25,178 @@ var (
 	DBCreateLock = new(sync.Mutex)
 )
 
-// SetupDB creates a database and sets .Database on the passed info.
-func SetupDB(info interface{}) error {
+const timeout = 10 * time.Second
+
+// Waits until no CREATE DATABASE or DROP DATABASE processes are listed
+// in master..sysprocesses.
+func waitForDBCreateDropProcess(ctx context.Context, db *sql.DB) error {
+	query := `select count(cmd)
+		from master..sysprocesses
+		where cmd like "CREATE DATABASE"
+			or cmd like "DROP DATABASE"`
+
+	queries := 0
+	timeoutCtx, cancel := context.WithTimeout(ctx, timeout)
+	defer cancel()
+
+	for {
+		row := db.QueryRowContext(timeoutCtx, query)
+
+		var count int
+		err := row.Scan(&count)
+		if err != nil && !errors.Is(err, sql.ErrNoRows) {
+			return fmt.Errorf("error scanning query result: %w", err)
+		}
+
+		if !errors.Is(err, sql.ErrNoRows) && count == 0 {
+			return nil
+		}
+
+		queries++
+		time.Sleep(
+			time.Duration(
+				int(math.Pow(2, float64(queries))) + rand.Intn(10),
+			),
+		)
+	}
+}
+
+func waitForDBInSysdatabases(ctx context.Context, db *sql.DB, dbname string) error {
+	query := `select count(name)
+		from master..sysdatabases
+		where name = "` + dbname + `"`
+
+	queries := 0
+	timeoutCtx, cancel := context.WithTimeout(ctx, timeout)
+	defer cancel()
+
+	for {
+		row := db.QueryRowContext(timeoutCtx, query)
+
+		var count int
+		err := row.Scan(&count)
+		if err != nil && !errors.Is(err, sql.ErrNoRows) {
+			return fmt.Errorf("error scanning query result: %w", err)
+		}
+
+		if !errors.Is(err, sql.ErrNoRows) && count == 1 {
+			return nil
+		}
+
+		queries++
+
+		time.Sleep(
+			time.Duration(
+				int(math.Pow(2, float64(queries))) + rand.Intn(10),
+			),
+		)
+	}
+}
+
+// SetupDB safely creates a database with the given name and sets
+// .Database on the passed info.
+func SetupDB(ctx context.Context, info interface{}, dbname string) error {
 	ttf := dsn.TagToField(info, dsn.OnlyJSON)
 	field, ok := ttf["database"]
 	if !ok {
-		return fmt.Errorf("provided info does not have the 'database' field")
+		return fmt.Errorf("integration: provided info does not have the 'database' field")
 	}
-
-	db, err := sql.Open("ase", dsn.FormatSimple(info))
-	if err != nil {
-		return fmt.Errorf("failed to open database: %w", err)
-	}
-	defer db.Close()
-
-	conn, err := db.Conn(context.Background())
-	if err != nil {
-		return fmt.Errorf("failed to open connection: %w", err)
-	}
-	defer conn.Close()
-
-	if _, err := conn.ExecContext(context.Background(), "use master"); err != nil {
-		return fmt.Errorf("failed to switch context to master: %w", err)
-	}
-
-	testDatabase := "test" + RandomNumber()
 
 	DBCreateLock.Lock()
 	defer DBCreateLock.Unlock()
 
-	if _, err := conn.ExecContext(context.Background(), fmt.Sprintf("if db_id('%s') is not null drop database %s", testDatabase, testDatabase)); err != nil {
-		return fmt.Errorf("error on conditional drop of database: %w", err)
-	}
-
-	if _, err := conn.ExecContext(context.Background(), "create database "+testDatabase); err != nil {
-		return fmt.Errorf("failed to create database: %w", err)
-	}
-
-	if _, err := conn.ExecContext(context.Background(), "use "+testDatabase); err != nil {
-		return fmt.Errorf("failed to switch context to %s: %w", testDatabase, err)
-	}
-
-	field.SetString(testDatabase)
-
-	return nil
-}
-
-// TeardownDB deletes the database indicated by .Database of the passed
-// info and unsets the member.
-func TeardownDB(info interface{}) error {
-	ttf := dsn.TagToField(info, dsn.OnlyJSON)
-	field, ok := ttf["database"]
-	if !ok {
-		return fmt.Errorf("provided info does not have the 'database' field")
-	}
-
 	db, err := sql.Open("ase", dsn.FormatSimple(info))
 	if err != nil {
-		return fmt.Errorf("failed to open database: %w", err)
+		return fmt.Errorf("integration: failed to open database: %w", err)
 	}
 	defer db.Close()
 
-	conn, err := db.Conn(context.Background())
+	if err := waitForDBCreateDropProcess(ctx, db); err != nil {
+		return fmt.Errorf("integration: error waiting for CREATE/DROP DATABASE processes: %w", err)
+	}
+
+	conn, err := db.Conn(ctx)
 	if err != nil {
-		return fmt.Errorf("failed to open connection: %w", err)
+		return fmt.Errorf("integration: failed to open connection: %w", err)
 	}
 	defer conn.Close()
 
-	if _, err := conn.ExecContext(context.Background(), "use master"); err != nil {
-		return fmt.Errorf("failed to switch context to master: %w", err)
+	if _, err := conn.ExecContext(ctx, "use master"); err != nil {
+		return fmt.Errorf("integration: failed to switch context to master: %w", err)
 	}
 
-	if _, err := conn.ExecContext(context.Background(), "drop database "+field.String()); err != nil {
-		return fmt.Errorf("failed to drop database: %w", err)
+	if _, err := conn.ExecContext(ctx, fmt.Sprintf("if db_id('%s') is not null drop database %s", dbname, dbname)); err != nil {
+		return fmt.Errorf("integration: error on conditional drop of database: %w", err)
 	}
 
+	if _, err := conn.ExecContext(ctx, "create database "+dbname); err != nil {
+		return fmt.Errorf("integration: failed to create database: %w", err)
+	}
+
+	// Wait for this CREATE process
+	if err := waitForDBCreateDropProcess(ctx, db); err != nil {
+		return fmt.Errorf("integration: error waiting for CREATE/DROP DATABASE processes: %w", err)
+	}
+
+	if err := waitForDBInSysdatabases(ctx, db, dbname); err != nil {
+		return fmt.Errorf("integration: error waiting for database %q to available in master..sysdatabases: %w", dbname, err)
+	}
+
+	if _, err := conn.ExecContext(ctx, "use "+dbname); err != nil {
+		return fmt.Errorf("integration: failed to switch context to %s: %w", dbname, err)
+	}
+
+	field.SetString(dbname)
+	return nil
+}
+
+// TeardownDB safely deletes the database indicated by .Database of the
+// passed info and unsets the member.
+func TeardownDB(ctx context.Context, info interface{}) error {
+	ttf := dsn.TagToField(info, dsn.OnlyJSON)
+	field, ok := ttf["database"]
+	if !ok {
+		return errors.New("integration: provided info does not have the 'database' field")
+	}
+
+	// Field must be unset beforehand to prevent issues with queries
+	// erroring because new connections cannot connect to the deleted
+	// database.
+	dbname := field.String()
 	field.SetString("")
+
+	DBCreateLock.Lock()
+	defer DBCreateLock.Unlock()
+
+	db, err := sql.Open("ase", dsn.FormatSimple(info))
+	if err != nil {
+		return fmt.Errorf("integration: failed to open database: %w", err)
+	}
+	defer db.Close()
+
+	// Wait for current CRETAE/DROP processes
+	if err := waitForDBCreateDropProcess(ctx, db); err != nil {
+		return fmt.Errorf("integration: error waiting for CREATE/DROP DATABASE processes: %w", err)
+	}
+
+	conn, err := db.Conn(ctx)
+	if err != nil {
+		return fmt.Errorf("integration: error getting db.Conn: %w", err)
+	}
+	defer conn.Close()
+
+	if _, err := conn.ExecContext(ctx, "use master"); err != nil {
+		return fmt.Errorf("integration: error switching to master: %w", err)
+	}
+
+	if _, err := conn.ExecContext(ctx, fmt.Sprintf("if db_id('%s') is not null drop database %s", dbname, dbname)); err != nil {
+		return fmt.Errorf("integration: error dropping database %q: %w", dbname, err)
+	}
+
+	// Wait for this DROP process
+	if err := waitForDBCreateDropProcess(ctx, db); err != nil {
+		return fmt.Errorf("integration: error waiting for CREATE/DROP DATABASE processes: %w", err)
+	}
 
 	return nil
 }
